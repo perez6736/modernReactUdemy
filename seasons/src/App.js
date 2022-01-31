0ad9c8cc39374c61251062645d9212c96aaaa7d8
Danny <perez6736@gmail.com>
2022-01-31 21:41:52 +0900

    functional vs class components

--- seasons/src/App.js
+++ seasons/src/App.js
@@ -1,25 +1,15 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import SeasonDisplay from "./SeasonDisplay";
 
-function App() {
-  return (
-    <div className="App test">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo " />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      (pos) => console.log(pos.coords.latitude),
+      (err) => console.log(err)
+    );
+
+    return <div> Latitude: </div>;
+  }
 }
 
 export default App;
---
+++ seasons/src/SeasonDisplay.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SeasonDisplay() {
+  return <div>display season here.</div>;
+}
+
+export default SeasonDisplay;
