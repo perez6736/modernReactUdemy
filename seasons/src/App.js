b51280432e75e05d1407be85097c634311ab1d68
Danny <perez6736@gmail.com>
2022-02-01 23:44:23 +0900

    learned about class componenet and its constructor

--- seasons/src/App.js
+++ seasons/src/App.js
@@ -2,6 +2,12 @@ import React from "react";
 import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { lat: null };
+  }
+
   render() {
     window.navigator.geolocation.getCurrentPosition(
       (pos) => console.log(pos.coords.latitude),
