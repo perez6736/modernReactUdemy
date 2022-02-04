95a9c1137d8dd91d9c12a0a0c52bc7b0e2e167cb
Danny <perez6736@gmail.com>
2022-02-04 13:50:57 +0900

    passing state as props adding logic to functional component

--- seasons/src/App.js
+++ seasons/src/App.js
@@ -26,7 +26,7 @@ class App extends React.Component {
     if (this.state.errorMessage && !this.state.lat) {
       return <div> Error: {this.state.errorMessage} </div>;
     } else if (!this.state.errorMessage && this.state.lat) {
-      return <div> Latitude: {this.state.lat} </div>;
+      return <SeasonDisplay lat={this.state.lat} />;
     } else {
       return <div> Loading </div>;
     }
--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,7 +1,34 @@
 import React from "react";
 
-function SeasonDisplay() {
-  return <div>display season here.</div>;
+const seasonConfig = {
+  summer: {
+    text: "Let's hit the beach!",
+    icon: "sun",
+  },
+  winter: {
+    text: "Burr it's icy Burr!",
+    icon: "snowflake",
+  },
+};
+
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat > 0 ? "winter" : "summer";
+  }
+};
+
+function SeasonDisplay(props) {
+  const season = getSeason(props.lat, new Date().getMonth());
+
+  return (
+    <div>
+      <i className={`icon ${season === "winter" ? "snowflake" : " sun"}`} />
+      <h1>{season === "summer" ? "Let's hit the beach." : "Burr it's icy."}</h1>
+      <i className={`icon ${season === "winter" ? "snowflake" : " sun"}`} />
+    </div>
+  );
 }
 
 export default SeasonDisplay;
