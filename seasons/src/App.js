4dc89bf7862fcd2073afcfe0469deae01f09c498
Danny <perez6736@gmail.com>
2022-02-04 12:06:19 +0900

    conditional jsx

--- seasons/src/App.js
+++ seasons/src/App.js
@@ -2,19 +2,34 @@ import React from "react";
 import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  // constructor(props) {
+  //   super(props);
 
-    this.state = { lat: null };
+  //   this.state = { lat: null, errorMessage: "" };
+  // }
+
+  state = { lat: null }; // after babel - this turns to a constructor and all
+
+  componentDidMount() {
+    console.log("my component was rendered to the screen.");
+    window.navigator.geolocation.getCurrentPosition(
+      (pos) => this.setState({ lat: pos.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
+    );
+  }
+
+  componentDidUpdate() {
+    console.log("my component was updated to the screen.");
   }
 
   render() {
-    window.navigator.geolocation.getCurrentPosition(
-      (pos) => console.log(pos.coords.latitude),
-      (err) => console.log(err)
-    );
-
-    return <div> Latitude: </div>;
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div> Error: {this.state.errorMessage} </div>;
+    } else if (!this.state.errorMessage && this.state.lat) {
+      return <div> Latitude: {this.state.lat} </div>;
+    } else {
+      return <div> Loading </div>;
+    }
   }
 }
 
