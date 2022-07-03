044ece37224eb16ebe26268de3224fb1476802a0
Danny <perez6736@gmail.com>
2022-07-03 22:40:17 +0900

    child to parent

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 
-function App() {
-  return (
-    <div className="ui container" style={{ marginTop: "10px" }}>
-      <SearchBar />
-    </div>
-  );
+class App extends React.Component {
+  OnSearchSubmit(term) {
+    console.log(term);
+  }
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <SearchBar onSubmit={this.OnSearchSubmit} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -3,10 +3,18 @@ import React from "react";
 class SearchBar extends React.Component {
   state = { term: "" };
 
+  onFormSubmit(event) {
+    event.preventDefault();
+    this.props.onSubmit(this.state.term);
+  }
+
   render() {
     return (
       <div className=" ui segment">
-        <form className="ui form">
+        <form
+          onSubmit={(event) => this.onFormSubmit(event)}
+          className="ui form"
+        >
           <div className="field">
             <label>Image Search </label>
             <input
