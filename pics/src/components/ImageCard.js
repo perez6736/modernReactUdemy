97b1a8de61fe0c8da6243a6ee993e0185fc0cbbc
Danny <perez6736@gmail.com>
2022-07-12 23:53:50 +0900

    react ref

--- pics/src/components/ImageCard.js
+++ pics/src/components/ImageCard.js
@@ -1,12 +1,26 @@
 import React from "react";
 
 class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.imageRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.imageRef.current.addEventListener("load", this.setSpans);
+  }
+
+  setSpans = () => {
+    console.log(this.imageRef.current.clientHeight);
+  };
+
   render() {
     const { alt_description, urls } = this.props.image;
 
     return (
       <div>
-        <img alt={alt_description} src={urls.regular} />
+        <img ref={this.imageRef} alt={alt_description} src={urls.regular} />
       </div>
     );
   }
