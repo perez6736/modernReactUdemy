2eefa080d99e4bc2aa36435176fe04cd9e46de9d
Danny <perez6736@gmail.com>
2023-04-20 00:10:24 +0900

    basic CRUD App in react

--- books/src/App.js
+++ books/src/App.js
@@ -1,15 +1,44 @@
 import { useState } from "react";
 import BookCreate from "./components/BookCreate";
+import BookList from "./components/BookList";
 
 function App() {
   const [books, setBooks] = useState([]);
 
   const createBook = (title) => {
-    console.log("need to add book with title -> " + title);
+    const updatedBooks = [
+      ...books,
+      { id: Math.round(Math.random() * 9999), title },
+    ];
+    setBooks(updatedBooks);
+  };
+
+  const deleteBookByID = (id) => {
+    const updatedBooks = books.filter((book) => {
+      return book.id !== id;
+    });
+
+    setBooks(updatedBooks);
+  };
+
+  const changeBookTitleByID = (id, newTitle) => {
+    const updatedBooks = books.map((book) => {
+      if (book.id === id) {
+        return { ...book, title: newTitle };
+      }
+      return book;
+    });
+    setBooks(updatedBooks);
   };
 
   return (
-    <div>
+    <div className="app">
+      <h1>ReadingList </h1>
+      <BookList
+        books={books}
+        onDelete={deleteBookByID}
+        onEdit={changeBookTitleByID}
+      />
       <BookCreate onCreate={createBook} />
     </div>
   );
