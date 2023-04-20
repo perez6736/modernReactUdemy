b9e40cc2a1099f672b8f51c21c8fbeae23c76425
Danny <perez6736@gmail.com>
2023-04-20 18:08:12 +0900

    using json server to mock an api

--- books/src/App.js
+++ books/src/App.js
@@ -1,15 +1,15 @@
 import { useState } from "react";
+import axios from "axios";
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
 
 function App() {
   const [books, setBooks] = useState([]);
 
-  const createBook = (title) => {
-    const updatedBooks = [
-      ...books,
-      { id: Math.round(Math.random() * 9999), title },
-    ];
+  const createBook = async (title) => {
+    const response = await axios.post("http://localhost:3001/books", { title });
+    console.log(response.data);
+    const updatedBooks = [...books, response.data];
     setBooks(updatedBooks);
   };
 
