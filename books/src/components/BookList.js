263c171b59289c80f2350bc4d902c976b8f0f3fd
Danny <perez6736@gmail.com>
2023-04-26 17:53:16 +0900

    usecallback hook practice

--- books/src/components/BookList.js
+++ books/src/components/BookList.js
@@ -1,9 +1,13 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import BooksContext from "../context/books";
 import BookShow from "./BookShow";
 
+function useBooksContext() {
+  return useContext(BooksContext);
+}
+
 function BookList() {
-  const { books } = useContext(BooksContext);
+  const { books } = useBooksContext(BooksContext);
 
   const renderedBooks = books.map((book) => {
     return <BookShow key={book.id} book={book} />;
