294aa71e44d718f788143cc0df997b9bbb138558
Danny <perez6736@gmail.com>
2023-04-27 12:06:34 +0900

    button theory and children

---
+++ comps/src/App.js
@@ -0,0 +1,28 @@
+import Button from "./Button";
+
+function App() {
+  return (
+    <div>
+      <div>
+        <Button> Plain </Button>
+      </div>
+      <div>
+        <Button> Primary </Button>
+      </div>
+      <div>
+        <Button> Secondary </Button>
+      </div>
+      <div>
+        <Button> Danger </Button>
+      </div>
+      <div>
+        <Button> Success </Button>
+      </div>
+      <div>
+        <Button> Warning </Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
