c1aba37032b945f7b906a524e8892f04c964ff1c
Danny <perez6736@gmail.com>
2023-05-11 18:09:14 +0900

    condtional rendering and event handlers

--- comps/src/components/Accordion.js
+++ comps/src/components/Accordion.js
@@ -1,9 +1,16 @@
+import { useState } from "react";
+
 function Accordion({ items }) {
-  const renderedItems = items.map((item) => {
+  const [expandedIndex, setExpandedIndex] = useState(0);
+
+  const renderedItems = items.map((item, index) => {
+    const isExpanded = index === expandedIndex;
+
     return (
-      <div className={item.id}>
-        <div>{item.label}</div>
-        <div>{item.content}</div>
+      <div key={item.id}>
+        <div onClick={() => setExpandedIndex(index)}>{item.label}</div>
+        {/* conditional rendering - && will return first falsey or last true. */}
+        {isExpanded && <div>{item.content}</div>}
       </div>
     );
   });
