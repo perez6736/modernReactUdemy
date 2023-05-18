0ac333cb235860bac16d4147955ee1de25b44e2f
Danny <perez6736@gmail.com>
2023-05-18 18:06:20 +0900

    start of dropdown component

--- comps/src/components/Dropdown.js
+++ comps/src/components/Dropdown.js
@@ -1,5 +1,33 @@
-function Dropdown() {
-  return <div>Dropdown...</div>;
+import { useState } from "react";
+
+function Dropdown({ options }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    //setIsOpen((currentIsOpen) => !currentIsOpen); - techinally correct to handle super fast repeat click
+    setIsOpen(!isOpen);
+  };
+
+  const handleOptionClick = (option) => {
+    //setIsOpen((currentIsOpen) => !currentIsOpen); - techinally correct to handle super fast repeat click
+    setIsOpen(false);
+    console.log(option);
+  };
+
+  const renderedOptions = options.map((option) => {
+    return (
+      <div key={option.value} onClick={() => handleOptionClick(option)}>
+        {option.label}
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <div onClick={handleClick}>Select...</div>
+      {isOpen && <div>{renderedOptions}</div>}
+    </div>
+  );
 }
 
 export default Dropdown;
