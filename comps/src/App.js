32b6a69d4f43adcfea4aed270372651b3b46b560
Danny <perez6736@gmail.com>
2023-05-19 20:54:00 +0900

    dropdown first iteration

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,12 +1,21 @@
 import Dropdown from "./components/Dropdown";
+import { useState } from "react";
 
 function App() {
+  const [selection, setSelection] = useState(null);
   const options = [
     { label: "Red", value: "red" },
     { label: "Orange", value: "orange" },
     { label: "Blue", value: "blue" },
   ];
-  return <Dropdown options={options} />;
+
+  const handleSelect = (option) => {
+    setSelection(option);
+  };
+
+  return (
+    <Dropdown onChange={handleSelect} options={options} value={selection} />
+  );
 }
 
 export default App;
