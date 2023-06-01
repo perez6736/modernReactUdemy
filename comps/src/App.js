359f3d39eb81b259108c8e1a3c788fde8aa80ba1
Danny <perez6736@gmail.com>
2023-06-01 12:37:59 +0900

    little bit of modal action

--- comps/src/App.js
+++ comps/src/App.js
@@ -4,6 +4,7 @@ import Route from "./components/Route";
 import AccordionPAge from "./pages/AccordionPage";
 import DropDownPage from "./pages/DropdownPage";
 import ButtonPage from "./pages/ButtonPage";
+import ModalPage from "./pages/ModalPage";
 
 function App() {
   return (
@@ -18,6 +19,9 @@ function App() {
         </Route>
         <Route path="/buttons">
           <ButtonPage></ButtonPage>
+        </Route>
+        <Route path="/modal">
+          <ModalPage></ModalPage>
         </Route>
       </div>
     </div>
