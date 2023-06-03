fd9f0135c1ab953934a7257c211ff4eaaf89304b
Danny <perez6736@gmail.com>
2023-06-03 18:42:06 +0900

    modal is done.

--- comps/src/pages/ModalPage.js
+++ comps/src/pages/ModalPage.js
@@ -8,12 +8,116 @@ function ModalPage() {
   const handleClick = () => {
     setShowModal(true);
   };
+
+  const handleClose = () => {
+    setShowModal(false);
+  };
+
+  const actionBar = (
+    <Button primary onClick={handleClose}>
+      Accept
+    </Button>
+  );
+  const modal = (
+    <Modal onClose={handleClose} actionBar={actionBar}>
+      <p>Here is something important in the modal.</p>
+    </Modal>
+  );
+
   return (
     <div>
       <Button primary onClick={handleClick}>
         Open Modal
       </Button>
-      {showModal && <Modal />}
+      {showModal && modal}
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed libero
+        orci, semper vitae urna sit amet, porttitor posuere leo. In faucibus
+        metus vel sem tempus, id dignissim erat suscipit. Vestibulum ac
+        efficitur ligula. Proin sodales ex nec suscipit vulputate. Ut nec
+        feugiat sapien. Etiam venenatis est felis, sed feugiat magna tincidunt
+        vel. Fusce rutrum scelerisque sapien. In convallis lectus nec
+        scelerisque facilisis. Ut eget nunc fringilla turpis molestie iaculis
+        quis in lectus. Donec a hendrerit ex. Duis et ipsum malesuada, fringilla
+        metus sit amet, vehicula justo.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed libero
+        orci, semper vitae urna sit amet, porttitor posuere leo. In faucibus
+        metus vel sem tempus, id dignissim erat suscipit. Vestibulum ac
+        efficitur ligula. Proin sodales ex nec suscipit vulputate. Ut nec
+        feugiat sapien. Etiam venenatis est felis, sed feugiat magna tincidunt
+        vel. Fusce rutrum scelerisque sapien. In convallis lectus nec
+        scelerisque facilisis. Ut eget nunc fringilla turpis molestie iaculis
+        quis in lectus. Donec a hendrerit ex. Duis et ipsum malesuada, fringilla
+        metus sit amet, vehicula justo.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed libero
+        orci, semper vitae urna sit amet, porttitor posuere leo. In faucibus
+        metus vel sem tempus, id dignissim erat suscipit. Vestibulum ac
+        efficitur ligula. Proin sodales ex nec suscipit vulputate. Ut nec
+        feugiat sapien. Etiam venenatis est felis, sed feugiat magna tincidunt
+        vel. Fusce rutrum scelerisque sapien. In convallis lectus nec
+        scelerisque facilisis. Ut eget nunc fringilla turpis molestie iaculis
+        quis in lectus. Donec a hendrerit ex. Duis et ipsum malesuada, fringilla
+        metus sit amet, vehicula justo.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed libero
+        orci, semper vitae urna sit amet, porttitor posuere leo. In faucibus
+        metus vel sem tempus, id dignissim erat suscipit. Vestibulum ac
+        efficitur ligula. Proin sodales ex nec suscipit vulputate. Ut nec
+        feugiat sapien. Etiam venenatis est felis, sed feugiat magna tincidunt
+        vel. Fusce rutrum scelerisque sapien. In convallis lectus nec
+        scelerisque facilisis. Ut eget nunc fringilla turpis molestie iaculis
+        quis in lectus. Donec a hendrerit ex. Duis et ipsum malesuada, fringilla
+        metus sit amet, vehicula justo.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed libero
+        orci, semper vitae urna sit amet, porttitor posuere leo. In faucibus
+        metus vel sem tempus, id dignissim erat suscipit. Vestibulum ac
+        efficitur ligula. Proin sodales ex nec suscipit vulputate. Ut nec
+        feugiat sapien. Etiam venenatis est felis, sed feugiat magna tincidunt
+        vel. Fusce rutrum scelerisque sapien. In convallis lectus nec
+        scelerisque facilisis. Ut eget nunc fringilla turpis molestie iaculis
+        quis in lectus. Donec a hendrerit ex. Duis et ipsum malesuada, fringilla
+        metus sit amet, vehicula justo.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed libero
+        orci, semper vitae urna sit amet, porttitor posuere leo. In faucibus
+        metus vel sem tempus, id dignissim erat suscipit. Vestibulum ac
+        efficitur ligula. Proin sodales ex nec suscipit vulputate. Ut nec
+        feugiat sapien. Etiam venenatis est felis, sed feugiat magna tincidunt
+        vel. Fusce rutrum scelerisque sapien. In convallis lectus nec
+        scelerisque facilisis. Ut eget nunc fringilla turpis molestie iaculis
+        quis in lectus. Donec a hendrerit ex. Duis et ipsum malesuada, fringilla
+        metus sit amet, vehicula justo.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed libero
+        orci, semper vitae urna sit amet, porttitor posuere leo. In faucibus
+        metus vel sem tempus, id dignissim erat suscipit. Vestibulum ac
+        efficitur ligula. Proin sodales ex nec suscipit vulputate. Ut nec
+        feugiat sapien. Etiam venenatis est felis, sed feugiat magna tincidunt
+        vel. Fusce rutrum scelerisque sapien. In convallis lectus nec
+        scelerisque facilisis. Ut eget nunc fringilla turpis molestie iaculis
+        quis in lectus. Donec a hendrerit ex. Duis et ipsum malesuada, fringilla
+        metus sit amet, vehicula justo.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed libero
+        orci, semper vitae urna sit amet, porttitor posuere leo. In faucibus
+        metus vel sem tempus, id dignissim erat suscipit. Vestibulum ac
+        efficitur ligula. Proin sodales ex nec suscipit vulputate. Ut nec
+        feugiat sapien. Etiam venenatis est felis, sed feugiat magna tincidunt
+        vel. Fusce rutrum scelerisque sapien. In convallis lectus nec
+        scelerisque facilisis. Ut eget nunc fringilla turpis molestie iaculis
+        quis in lectus. Donec a hendrerit ex. Duis et ipsum malesuada, fringilla
+        metus sit amet, vehicula justo.
+      </p>
     </div>
   );
 }
