e354932f5e87be286e02c9cbd1cd66e541f74d43
Danny <perez6736@gmail.com>
2023-06-12 00:26:37 +0900

    sortable table is complete

--- comps/src/components/SortableTable.js
+++ comps/src/components/SortableTable.js
@@ -1,13 +1,35 @@
 import Table from "./Table";
 import { useState } from "react";
+import { GoArrowSmallDown, GoArrowSmallUp } from "react-icons/go";
 
 function SortableTable(props) {
-  const { config } = props;
+  const { config, data } = props;
   const [sortOrder, setSortOrder] = useState(null);
   const [sortBy, setSortBy] = useState(null);
 
+  let sortedData = data;
+  if (sortOrder && sortBy) {
+    const { sortValue } = config.find((col) => col.label === sortBy);
+    sortedData = [...data].sort((a, b) => {
+      const valueA = sortValue(a);
+      const valueB = sortValue(b);
+      const reverseOrder = sortOrder === "asc" ? 1 : -1;
+
+      if (typeof valueA === "string") {
+        return valueA.localeCompare(valueB) * reverseOrder;
+      } else {
+        return (valueA - valueB) * reverseOrder;
+      }
+    });
+  }
+
   const handleClick = (label) => {
-    console.log(sortOrder);
+    if (sortBy && label !== sortBy) {
+      setSortOrder("asc");
+      setSortBy(label);
+      return;
+    }
+
     if (sortOrder === null) {
       setSortOrder("asc");
       setSortBy(label);
@@ -23,20 +45,53 @@ function SortableTable(props) {
   const updatedConfig = config.map((col) => {
     if (!col.sortValue) return col;
 
+    const getIcons = (label, sortBy, sortOrder) => {
+      if (label !== sortBy) {
+        return (
+          <div>
+            <GoArrowSmallUp /> <GoArrowSmallDown />
+          </div>
+        );
+      }
+
+      if (sortOrder === null) {
+        return (
+          <div>
+            <GoArrowSmallUp /> <GoArrowSmallDown />
+          </div>
+        );
+      } else if (sortOrder === "asc") {
+        return (
+          <div>
+            <GoArrowSmallUp />
+          </div>
+        );
+      } else if (sortOrder === "desc") {
+        return (
+          <div>
+            <GoArrowSmallDown />
+          </div>
+        );
+      }
+    };
+
     return {
       ...col,
       header: () => (
-        <th onClick={() => handleClick(col.label)}>{col.label}IS SORTABALE</th>
+        <th
+          className="cursor-pointer hover:bg-gray-100"
+          onClick={() => handleClick(col.label)}
+        >
+          <div className="flex items-cetner">
+            {col.label}
+            {getIcons(col.label, sortBy, sortOrder)}
+          </div>
+        </th>
       ),
     };
   });
 
-  return (
-    <div>
-      {sortBy} - {sortOrder}
-      <Table {...props} config={updatedConfig} />
-    </div>
-  );
+  return <Table {...props} data={sortedData} config={updatedConfig} />;
 }
 
 export default SortableTable;
--- comps/src/pages/TablePage.js
+++ comps/src/pages/TablePage.js
@@ -29,24 +29,6 @@ function TablePage() {
 
   const keyFn = (fruit) => fruit.name;
 
-  function getSortValue(fruit) {
-    return fruit.name;
-  }
-
-  const sortOrder = "asc";
-  data.sort((a, b) => {
-    const valueA = getSortValue(a);
-    const valueB = getSortValue(b);
-
-    const reverseOrder = sortOrder === "asc" ? 1 : -1;
-
-    if (typeof valueA === "string") {
-      return valueA.localeCompare(valueB) * reverseOrder;
-    } else {
-      return (valueA - valueB) * reverseOrder;
-    }
-  });
-
   return (
     <SortableTable data={data} config={config} keyFn={keyFn}>
       Table
