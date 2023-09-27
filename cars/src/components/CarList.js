8508dd1cf4106f8aec157b90eb2697a82f95f737
Danny <perez6736@gmail.com>
2023-09-27 23:50:45 +0900

    display a list from state

--- cars/src/components/CarList.js
+++ cars/src/components/CarList.js
@@ -4,7 +4,30 @@ function CarList() {
   const cars = useSelector((state) => {
     return state.cars.data;
   });
-  return <div>CarList</div>;
+
+  const handleCarDelete = () => {};
+
+  const renderedCars = cars.map((car) => {
+    return (
+      <div key={car.id} className="panel">
+        <p>
+          {car.name} - ${car.cost}
+        </p>
+        <button
+          onClick={() => handleCarDelete(car)}
+          className="button is-danger"
+        >
+          Delete
+        </button>
+      </div>
+    );
+  });
+  return (
+    <div className="car-list">
+      {renderedCars}
+      <hr />
+    </div>
+  );
 }
 
 export default CarList;
