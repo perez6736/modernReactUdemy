7dc92dd2c3944806efee374ff9f9f94ebc35c6c9
Danny <perez6736@gmail.com>
2024-02-21 22:46:42 +0900

    setting up redux store for section 21

--- media/src/App.js
+++ media/src/App.js
@@ -1,5 +1,11 @@
+import UsersList from "./components/UsersList";
+
 function App() {
-  return "app! ";
+  return (
+    <div className="container mx-auto">
+      <UsersList />
+    </div>
+  );
 }
 
 export default App;
