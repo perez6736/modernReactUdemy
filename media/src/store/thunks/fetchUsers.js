570f10d763e8fe6eb14376950327f9e7e258555e
Danny <perez6736@gmail.com>
2024-03-03 22:49:34 +0900

    skeleton loader with animation

--- media/src/store/thunks/fetchUsers.js
+++ media/src/store/thunks/fetchUsers.js
@@ -6,7 +6,15 @@ const fetchUsers = createAsyncThunk("users/fetch", async () => {
   //request logic goes here.
   const response = await axios.get("http://localhost:3001/users");
 
+  await pause(5000); //dev only - to test the loading screen.
   return response.data;
 });
+
+//dev helper function
+const pause = (duration) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, duration);
+  });
+};
 
 export { fetchUsers };
