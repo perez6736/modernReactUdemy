45a16aa282e2e4d25e0f63cf2e6505d02be63506
Danny <perez6736@gmail.com>
2024-05-03 23:22:12 +0900

    delete users

--- media/src/store/slices/usersSlice.js
+++ media/src/store/slices/usersSlice.js
@@ -1,6 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { fetchUsers } from "../thunks/fetchUsers";
 import { addUser } from "../thunks/addUser";
+import { deleteUser } from "../thunks/deleteUser";
 
 const usersSlice = createSlice({
   name: "users",
@@ -32,6 +33,20 @@ const usersSlice = createSlice({
       state.data.push(action.payload); // push the payload(what the api returned) into state.
     });
     builder.addCase(addUser.rejected, (state, action) => {
+      state.isLoading = false;
+      state.error = action.error;
+    });
+    //adduser Thunks
+    builder.addCase(deleteUser.pending, (state, action) => {
+      state.isLoading = true;
+    });
+    builder.addCase(deleteUser.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.data = state.data.filter((user) => {
+        return user.id !== action.payload.id;
+      });
+    });
+    builder.addCase(deleteUser.rejected, (state, action) => {
       state.isLoading = false;
       state.error = action.error;
     });
