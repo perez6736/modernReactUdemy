73137055b94ab7212922f99e43c483424a4bdc26
Danny <perez6736@gmail.com>
2024-05-03 20:18:11 +0900

    hook to change button to loading state

--- media/src/components/UsersList.js
+++ media/src/components/UsersList.js
@@ -1,27 +1,24 @@
-import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
 import { fetchUsers, addUser } from "../store";
 import Button from "./Button";
 import Skeleton from "./skeleton";
+import { useThunk } from "../hooks/use-thunk";
 
 function UsersList() {
-  const [isLoadingUsers, setLoadingUsers] = useState(false);
-  const [loadingUsersError, setLoadingUsersError] = useState(null);
-  const dispatch = useDispatch();
+  const [doFetchUsers, isLoadingUsers, loadingUsersError] =
+    useThunk(fetchUsers);
+  const [doCreateUser, isCreatingUser, creatingUserError] = useThunk(addUser);
   const { data } = useSelector((state) => {
     return state.users; // will grab state from users
   });
 
   useEffect(() => {
-    setLoadingUsers(true);
-    dispatch(fetchUsers())
-      .unwrap()
-      .catch(() => setLoadingUsersError(true))
-      .finally(() => setLoadingUsers(false));
-  }, [dispatch]);
+    doFetchUsers();
+  }, [doFetchUsers]);
 
   const handleUserAdd = () => {
-    dispatch(addUser());
+    doCreateUser();
   };
 
   if (isLoadingUsers) {
@@ -45,7 +42,12 @@ function UsersList() {
     <div>
       <div className="flex flex-row justify-between m-3">
         <h1 className="m-2 text-xl">Users</h1>
-        <Button onClick={handleUserAdd}>+add User</Button>
+        {isCreatingUser ? (
+          "creating user ..."
+        ) : (
+          <Button onClick={handleUserAdd}>+add User</Button>
+        )}
+        {creatingUserError && "Error creating user..."}
       </div>
       {renderedUsers}
     </div>
