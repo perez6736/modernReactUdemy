79667322134f52cb2919947536f66c5c65794297
Danny <perez6736@gmail.com>
2024-09-08 21:20:41 +0900

    create type interface for loader results

--- registry/src/pages/search/SearchPage.tsx
+++ registry/src/pages/search/SearchPage.tsx
@@ -1,7 +1,8 @@
 import { useLoaderData } from "react-router-dom";
+import { SearchLoaderResult } from "./searchLoader";
 
 export default function SearchPage() {
-  const data = useLoaderData();
+  const data = useLoaderData() as SearchLoaderResult;
   console.log(data);
   return <div>SearchPage</div>;
 }
